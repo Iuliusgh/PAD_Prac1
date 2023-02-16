68b701a5479ff3f84d11dc634178970c60a56d9c
petio01bg <81573779+petio01bg@users.noreply.github.com>
2023-02-16 12:37:04 +0100

    Prac1

---
+++ Calculator/app/src/test/java/es/ucm/fdi/calculator/CalculatorUnitTest.java
@@ -0,0 +1,12 @@
+package es.ucm.fdi.calculator;
+
+import org.junit.Test;
+
+import es.ucm.fdi.calculator.Suma;
+
+public class CalculatorUnitTest {
+    @Test
+    public void suma(){
+        double sum = new Suma(1,2);
+    }
+}
