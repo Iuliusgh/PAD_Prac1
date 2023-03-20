4c8db713f4b1113b85721d2359c963b6fe5ac9f8
YuejieXuUCM <xuyuejie45@gmail.com>
2023-03-20 20:14:36 +0100

    La interfaz de perfil y de ajustes

--- EducaVial/app/src/main/java/com/example/educavial/MainActivity.java
+++ EducaVial/app/src/main/java/com/example/educavial/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.educavial;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton settings, profile;
+
+    private final static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        Log.d(TAG, "Inicializando variables");
+        settings = findViewById(R.id.ajustes);
+        profile = findViewById(R.id.perfil);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                config();
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perfil();
+            }
+        });
+    }
+
+    private void config(){
+        Intent i = new Intent(this, AjustesActivity.class);
+        startActivity(i);
+    }
+
+    private void perfil(){
+        Intent i = new Intent(this, PerfilActivity.class);
+        startActivity(i);
     }
 }
