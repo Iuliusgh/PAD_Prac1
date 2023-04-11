56c2162eafa890846f94b344808c730b59c010e4
Iuliusgh <iuliusgh@ucm.es>
2023-04-11 20:44:55 +0200

    prueba commit desde as

---
+++ EducaVial/app/src/main/java/com/example/educavial/ListaSenalesActivity.java
@@ -0,0 +1,41 @@
+package com.example.educavial;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ListaSenalesActivity extends AppCompatActivity {
+    private AlertDialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_senales);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.volver);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.learnt_trafic_sign);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.help:
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
