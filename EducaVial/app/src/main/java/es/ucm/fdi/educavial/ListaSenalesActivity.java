8fe629eb1a8fe009bc0c5d4a6350854aaf203158
Iuliusgh <iuliusgh@ucm.es>
2023-04-24 17:30:57 +0200

    lista señales con texto bien

--- EducaVial/app/src/main/java/es/ucm/fdi/educavial/ListaSenalesActivity.java
+++ EducaVial/app/src/main/java/es/ucm/fdi/educavial/ListaSenalesActivity.java
@@ -1,18 +1,19 @@
 package es.ucm.fdi.educavial;
 
+
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,11 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class ListaSenalesActivity extends AppCompatActivity {
     private AlertDialog dialog;
@@ -44,14 +50,16 @@ public class ListaSenalesActivity extends AppCompatActivity {
                 row.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                 for (int j=0;j<3;j++){
                     AppCompatButton senal =  new AppCompatButton(this);
+                    String signalCode=senalList.get(i*3+j).codigo.toLowerCase().replace("-","");
+                    setImageFromAPI(senal,signalCode);
                     senal.setCompoundDrawablesWithIntrinsicBounds(null, AppCompatResources.getDrawable(this,R.drawable.placeholder),null,null);
                     senal.setText(senalList.get(i*3+j).nombre);
-                    //senal.setText("Señal");
                     senal.setBackgroundColor(Color.TRANSPARENT);
                     senal.setTextColor(Color.BLACK);
-                    senal.setTextSize(5);
+                    senal.setMaxLines(5);
+                    senal.setTextSize(10);
                     senal.setLayoutParams(new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            0,
                             LinearLayout.LayoutParams.WRAP_CONTENT
                             ,1));
                     row.addView(senal);
@@ -77,5 +85,23 @@ public class ListaSenalesActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void setImageFromAPI(AppCompatButton button, String signalCode){
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                String APIResult=NetworkUtils.getSignalInfoJson(signalCode);
+                try {
+                    JSONObject jsonObject = new JSONObject(APIResult);
+                    JSONArray listOfItems = (JSONArray) jsonObject.get("query");
+                    int a = 3;
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+
+
     }
 }
--- EducaVial/app/src/main/java/es/ucm/fdi/educavial/NetworkUtils.java
+++ EducaVial/app/src/main/java/es/ucm/fdi/educavial/NetworkUtils.java
@@ -10,23 +10,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class NetworkUtils {
     private static final String TAG = "NetworkUtils";
     private static final String BASE_URL =
-            "https://es.wikipedia.org/w/api.php?action=query";
+            "https://es.wikipedia.org/w/api.php?";
     private static final String QUERY_PARAM = "action";
-    private static final String MAX_RESULTS = "maxResults";
-    private static final String PRINT_TYPE = "printType";
+    private static final String TITLES = "titles";
+    private static final String PROP = "prop";
+    private static final String FORMAT = "format";
+    //https://en.wikipedia.org/w/api.php?action=query&titles=File:Spain_traffic_signal_s14a.svg&prop=imageinfo&iiprop=url&format=json
 
-    static String getBookInfoJson(String queryString, String printType){
+    static String getSignalInfoJson(String signalName){
         HttpURLConnection conn = null;
         InputStream is = null;
         String res = null;
         Uri builtURI = Uri.parse(BASE_URL).buildUpon()
-                .appendQueryParameter(QUERY_PARAM, queryString)
-                .appendQueryParameter(MAX_RESULTS, "40")
-                .appendQueryParameter(PRINT_TYPE, printType)
+                .appendQueryParameter(QUERY_PARAM, "query")
+                .appendQueryParameter(TITLES, "File"+Uri.decode(":")+"Spain_traffic_signal_"+signalName+".svg")
+                .appendQueryParameter(PROP, "url")
+                .appendQueryParameter(FORMAT, "json")
                 .build();
         try {
             URL requestURL = new URL(builtURI.toString());
@@ -45,7 +49,7 @@ public class NetworkUtils {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line;
             while ((line = reader.readLine()) != null) {
-                builder.append(line + "\n");
+                builder.append(line).append("\n");
             }
             if (builder.length() == 0) {
                 return null;
