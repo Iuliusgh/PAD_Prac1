dd2e2bab26527aabb1be67bb733e29bc1f8a7b15
Iván zhuo <124189749+IvanZhuo01@users.noreply.github.com>
2023-04-26 18:51:20 +0200

    Eliminado cosas

--- EducaVial/app/src/main/java/es/ucm/fdi/educavial/Database.java
+++ EducaVial/app/src/main/java/es/ucm/fdi/educavial/Database.java
@@ -4,10 +4,9 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import android.content.Context;
 
-@androidx.room.Database(entities = {User.class, Tema.class,Senal.class},version = 2)
+@androidx.room.Database(entities = {Senal.class},version = 1)
 public abstract class Database extends RoomDatabase {
-    public abstract UserDAO userDAO();
-    public abstract TemaDAO temaDAO();
+
     public abstract SenalDAO SenalDAO();
     public static Database instance;
     public static Database getDatabase(final Context context){
