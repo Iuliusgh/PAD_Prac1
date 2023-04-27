f0593ac57f01be09796305d03372aa0fa72de18e
YuejieXuUCM <xuyuejie45@gmail.com>
2023-04-27 17:56:10 +0200

    Modificaciones
    - Funcionamiento del ListaSenalesActivity cuando no hay conexión
    - TemxaXActivity parar de reproducir el texto cuando se pulsa siguiente/anterior.
    - Eliminación del hueco presentes en los xml

--- EducaVial/app/src/main/java/es/ucm/fdi/educavial/TemaXActivity.java
+++ EducaVial/app/src/main/java/es/ucm/fdi/educavial/TemaXActivity.java
@@ -92,6 +92,7 @@ public class TemaXActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "Botón anterior pulsado");
+                stopRepro();
                 pos--;
                 if(pos == 0){
                     ant.setEnabled(false);
@@ -107,6 +108,7 @@ public class TemaXActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "Botón siguiente pulsado");
+                stopRepro();
                 pos++;
                 bar.setProgress( bar.getProgress() + barValue);
                 if(pos == cont.size()){
@@ -203,12 +205,16 @@ public class TemaXActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void stopRepro(){
+        if(textToSpeech != null){
+            textToSpeech.shutdown();
+        }
+    }
+
     @Override
     protected void onStop()
     {
         super.onStop();
-        if(textToSpeech != null){
-            textToSpeech.shutdown();
-        }
+        stopRepro();
     }
 }
